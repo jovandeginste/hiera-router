109e3bbd1a052a30087f237f02246b385a6cf668
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2016-06-22 18:04:02 +0200

    First (working) version with README
    Todo: make it a gem

---
+++ README.md
@@ -0,0 +1,89 @@
+# Hiera 'router' backend
+
+This hiera backend is a drop-in replacement for default yaml, and will reroute requests to different hiera backends based on the value returned by the yaml files.
+
+## Example
+
+Content of `./hiera.yaml`:
+
+```yaml
+:backends:
+  - router
+:logger: console
+:hierarchy:
+  - level1
+  - level2
+:router:
+  :datadir: ./hieradata/
+  :backends:
+    - vault
+:vault:
+  :ssl_verify: false
+  :addr: https://active.vault.service.consul:8200
+  :token: mytoken
+  :mounts:
+    :generic:
+      - secret/puppet
+```
+
+Content of `./hieradata/level1.yaml`:
+
+```yaml
+mykey: backend[vault]
+mykey2: backend[vault]
+```
+
+Content of `./hieradata/level2.yaml`:
+
+```yaml
+mykey:
+  hiera-value: 25
+  other-hiera-value: xyz
+mykey2: some_value
+```
+
+And a `vault` server setup so that `mytoken` has read access to `secret/puppet`, and contains a key
+`secret/teams/systeembeheer-unix/puppet/level1/mykey` with values:
+
+```yaml
+vault-value: a
+other-vault-value: 2
+```
+
+This example will find 'look in vaul' value in `level1.yaml`, try to look in vault, will return empty handed and thus
+look further in the yaml tree and find `some_value` in `level2.yaml`:
+
+```
+$ hiera -c hiera.yaml mykey2
+some_value
+```
+
+Request a string, so no merging will happen. First value found is 'look in vault', which has a value for this key:
+
+```
+$ hiera -c hiera.yaml mykey
+{"mykey"=>"a", "tweede"=>"2"}
+```
+
+Request a hash, so merging will happen. First value found is 'look in vault', which has a value for this key. Another set of values is found in `level2.yaml`, which are added:
+
+```
+$ hiera -c hiera.yaml -h mykey # Request a hash, so merging will happen
+{"hiera-value"=>25, "other-hiera-value"=>"xyz", "vault-value"=>"a", "other-vault-value"=>"2"}
+```
+
+In Ruby code:
+
+```ruby
+require 'hiera'
+backend = Hiera.new(:config => 'hiera.yaml')
+
+puts backend.lookup("mykey", "mydefault", {}, nil, :string).inspect
+# result: {"mykey"=>"a", "tweede"=>"2"}
+
+puts backend.lookup("mykey", "mydefault", {}, nil, :hash).inspect
+# result: {"hiera-value"=>25, "other-hiera-value"=>"xyz", "vault-value"=>"a", "other-vault-value"=>"2"}
+
+puts backend.lookup("mykey2", "mydefault", {}, nil, :string).inspect
+# result: "some_value"
+```
---
+++ lib/hiera/backend/router_backend.rb
@@ -0,0 +1,107 @@
+class Hiera
+	module Backend
+		class Router_backend
+			attr_reader :backends
+
+			def initialize(cache = nil)
+				require 'hiera/filecache'
+				require 'yaml'
+				@cache = cache || Filecache.new
+				@backends = {}
+				Config[:router][:backends].each do |backend|
+					require "hiera/backend/#{backend.downcase}_backend"
+					@backends[backend] = Hiera::Backend.const_get("#{backend.capitalize}_backend").new
+				end
+
+				Hiera.debug("hiera router initialized")
+			end
+			def lookup(key, scope, order_override, resolution_type)
+				options = {
+					:key => key,
+					:scope => scope,
+					:order_override => order_override,
+					:resolution_type => resolution_type,
+				}
+				answer = nil
+
+				Hiera.debug("Looking up #{key} in yaml backend")
+
+				Backend.datasources(scope, order_override) do |source|
+					Hiera.debug("Looking for data source #{source}")
+					yaml_file = Backend.datafile(:router, scope, source, 'yaml') || next
+
+					next unless File.exists?(yaml_file)
+
+					data = @cache.read(yaml_file, Hash) do |cached_data|
+						YAML.load(cached_data) || {}
+					end
+
+					next if data.empty?
+					next unless data.include?(key)
+
+					Hiera.debug("Found #{key} in #{source}")
+
+					new_answer = parse_answer(data[key], scope, options)
+					next unless new_answer
+
+					case resolution_type
+					when :array
+						raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
+						answer ||= []
+						answer << new_answer
+					when :hash
+						raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+						answer ||= {}
+						answer = Backend.merge_answer(new_answer,answer)
+					else
+						answer = new_answer
+						break
+					end
+				end
+
+				return answer
+			end
+
+			def parse_answer(data, scope, options)
+				if data.is_a?(Numeric) or data.is_a?(TrueClass) or data.is_a?(FalseClass)
+					return data
+				elsif data.is_a?(String)
+					return parse_string(data, scope, options)
+				elsif data.is_a?(Hash)
+					answer = {}
+					data.each_pair do |key, val|
+						interpolated_key = Backend.parse_string(key, scope)
+						answer[interpolated_key] = parse_answer(val, scope, options)
+					end
+
+					return answer
+				elsif data.is_a?(Array)
+					answer = []
+					data.each do |item|
+						answer << parse_answer(item, scope, options)
+					end
+
+					return answer
+				end
+			end
+
+			def parse_string(data, scope, options)
+				if match = data.match(/^backend\[([^,]+)(?:,(.*))?\]$/)
+					backend_name, backend_parameters = match.captures
+					backend_options = options
+					backend_options = backend_options.merge(backend_parameters) if backend_parameters
+					Hiera.debug "Calling hiera with '#{backend_name}'..."
+					if backend = backends[backend_name]
+						result = backend.lookup(backend_options[:key], backend_options[:scope], nil, backend_options[:resolution_type])
+					else
+						raise "Backend '#{backend_name}' was not configured."
+					end
+					Hiera.debug "Call to '#{backend_name}' finished."
+					return result
+				else
+					Backend.parse_string(data, scope)
+				end
+			end
+		end
+	end
+end
